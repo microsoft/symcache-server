1bf66674e1911e2930c2616e7566ffc9260d0eef
davidmatson <opencode@outlook.com>
2020-01-26 19:58:07 -0800

    Add initial sample.

---
+++ SymCacheHttpServer/Startup.cs
@@ -0,0 +1,90 @@
+﻿// © Microsoft Corporation. All rights reserved.
+
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SymCacheHttpServer;
+using System;
+using System.IO;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+            .AddNegotiate();
+
+        services.AddSingleton<SymCacheRepository>();
+        services.AddSingleton<SymCacheTranscoder>();
+        services.AddSingleton<SymbolServerClient>();
+        services.AddSingleton<BackgroundTranscodeService>();
+        services.AddSingleton<IBackgroundTranscodeQueue>(sp => sp.GetRequiredService<BackgroundTranscodeService>());
+        services.AddSingleton<IHostedService>(sp => sp.GetRequiredService<BackgroundTranscodeService>());
+
+        Uri symbolServer = new Uri(GetValueOrThrow("SymbolServer"));
+        string symCacheDirectory = GetValueOrThrow("SymCacheDirectory");
+
+        if (!Directory.Exists(symCacheDirectory))
+        {
+            throw new DirectoryNotFoundException(symCacheDirectory);
+        }
+
+        string transcoderPath = GetValueOrThrow("TranscoderPath");
+
+        if (!File.Exists(transcoderPath))
+        {
+            throw new FileNotFoundException(transcoderPath);
+        }
+
+        SemanticVersion transcoderVersion = SemanticVersion.Parse(GetValueOrThrow("TranscoderVersion"));
+
+        services.Configure<SymCacheOptions>(o =>
+        {
+            o.SymbolServer = symbolServer;
+            o.SymCacheDirectory = symCacheDirectory;
+            o.TranscoderPath = transcoderPath;
+            o.TranscoderVersion = transcoderVersion;
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+
+    string GetValueOrThrow(string name)
+    {
+        string value = Configuration[name];
+
+        if (value == null)
+        {
+            throw new InvalidOperationException($"{name} is a required configuration parameter. " +
+                $"Please set an environment variable with an appropriate value.");
+        }
+
+        return value;
+    }
+}
---
+++ SymCacheHttpServer/SymCacheRepository.cs
@@ -0,0 +1,82 @@
+﻿// © Microsoft Corporation. All rights reserved.
+
+using Microsoft.Extensions.Options;
+using System;
+using System.IO;
+
+public class SymCacheRepository
+{
+    readonly string directory;
+
+    public SymCacheRepository(IOptions<SymCacheOptions> options)
+    {
+        directory = Path.GetFullPath(options.Value.SymCacheDirectory);
+    }
+
+    public CacheResult Find(SymCacheKey key)
+    {
+        string path = GetPath(key);
+
+        if (File.Exists(path))
+        {
+            return new CacheResult(CacheStatus.PositivelyCached, path, key.Version);
+        }
+
+        string negativeCachePath = GetNegativeCachePath(key);
+
+        if (File.Exists(negativeCachePath))
+        {
+            try
+            {
+                DateTimeOffset negativeCacheExpires = DateTimeOffset.Parse(File.ReadAllText(negativeCachePath));
+
+                if (negativeCacheExpires.UtcDateTime < DateTimeOffset.UtcNow)
+                {
+                    File.Delete(negativeCachePath);
+                }
+
+                return new CacheResult(CacheStatus.NegativelyCached, null, null);
+            }
+            catch
+            {
+                // Concurrent requests could mean the file read or delete above fails. Treat the negative cache entry as
+                // non-existent in that case.
+            }
+        }
+
+        return new CacheResult(CacheStatus.NotCached, null, null);
+    }
+
+    public void MarkNegativelyCached(SymCacheKey key)
+    {
+        // Treat negative cache entry as valid for 1 day.
+        string contents = DateTimeOffset.UtcNow.AddDays(1).ToString("o");
+        string relativePath = GetNegativeCacheRelativePath(key);
+        string path = Path.Combine(directory, relativePath);
+
+        // Ensure parent directories exist.
+        Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+        File.WriteAllText(path, contents);
+    }
+
+    public string GetPath(SymCacheKey key)
+    {
+        return Path.Combine(directory, GetRelativePath(key));
+    }
+
+    string GetNegativeCachePath(SymCacheKey key)
+    {
+        return Path.Combine(directory, GetNegativeCacheRelativePath(key));
+    }
+
+    internal static string GetRelativePath(SymCacheKey key)
+    {
+        return $@"{key.PdbName}\{key.PdbId:N}{key.PdbAge:X}\{key.PdbName}-v{key.Version}.symcache";
+    }
+
+    static string GetNegativeCacheRelativePath(SymCacheKey key)
+    {
+        return Path.ChangeExtension(GetRelativePath(key), "negativesymcache");
+    }
+}
