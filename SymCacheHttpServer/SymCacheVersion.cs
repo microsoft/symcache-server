e3b92055befbe37f5b45b90da526e3c470a123fa
davidmatson <opencode@outlook.com>
2020-01-26 19:58:07 -0800

    Add initial sample.

---
+++ SymCacheHttpServer/SymCacheVersion.cs
@@ -0,0 +1,11 @@
+﻿// © Microsoft Corporation. All rights reserved.
+
+static class SymCacheVersion
+{
+    // This sample only shows how to handle requests for v3.0.0+ SymCache files.
+    public static readonly SemanticVersion MinVersion = new SemanticVersion(3, 0, 0);
+
+    // Always do transcode operations asynchronously if the requested version is greater than v3.1.0. All v3.1.0 clients
+    // after v3.1.0 support asynchronous transcodes.
+    public static readonly SemanticVersion AlwaysAsyncTranscodeAfterVersion = new SemanticVersion(3, 1, 0);
+}
