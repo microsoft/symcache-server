25a32c30f59e85cd5411b989f995e2d41410fefd
davidmatson <opencode@outlook.com>
2020-01-26 19:58:07 -0800

    Add initial sample.

---
+++ SymCacheHttpServer/CacheResult.cs
@@ -0,0 +1,15 @@
+﻿// © Microsoft Corporation. All rights reserved.
+
+public class CacheResult
+{
+    public CacheResult(CacheStatus status, string path, SemanticVersion version)
+    {
+        Status = status;
+        Path = path;
+        Version = version;
+    }
+
+    public CacheStatus Status { get; }
+    public string Path { get; }
+    public SemanticVersion Version { get; }
+}
---
+++ SymCacheHttpServer/NotFoundRetryAfterResult.cs
@@ -0,0 +1,23 @@
+﻿// © Microsoft Corporation. All rights reserved.
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Globalization;
+
+class NotFoundRetryAfterResult : NotFoundResult
+{
+    public NotFoundRetryAfterResult(TimeSpan delay)
+    {
+        Delay = delay;
+    }
+
+    public TimeSpan Delay { get; }
+
+    public override void ExecuteResult(ActionContext context)
+    {
+        base.ExecuteResult(context);
+        context.HttpContext.Response.Headers[HeaderNames.RetryAfter] =
+            unchecked((long)Delay.TotalSeconds).ToString(CultureInfo.InvariantCulture);
+    }
+}
